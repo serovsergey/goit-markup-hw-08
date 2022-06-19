ca1fe305275a2721b75f97d507203e1f57caa671
Serov Sergey <serovsergey79@gmail.com>
2022-06-20 02:46:11 +0300

    add: mobile version

---
+++ js/main.js
@@ -0,0 +1,26 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelector("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        menuBtn: document.querySelector("[data-menu-button]"),
+        mobileMenu: document.querySelector("[data-menu]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.menuBtn.addEventListener('click', toggleMenu);
+
+    function toggleMenu() {
+        const expanded = refs.menuBtn.getAttribute("aria-expanded") == "true" | false;
+        refs.menuBtn.classList.toggle("is-open");
+        refs.menuBtn.setAttribute("aria-expanded", !expanded);
+        refs.mobileMenu.classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll");
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+    }
+})();
